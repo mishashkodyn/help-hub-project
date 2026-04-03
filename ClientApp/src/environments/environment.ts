f1e7baed09f88f11f75e9461c7839112868f253b
Mykhaylo Shkodyn <ms@a2software.dk>
2026-04-04 01:49:38 +0300

    /

--- ClientApp/src/environments/environment.ts
+++ ClientApp/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  hubUrl: 'http://helphub-platform.fun/hubs',
-  apiBaseUrl: 'http://helphub-platform.fun/api'
+  hubUrl: '/hubs',
+  apiBaseUrl: '/api'
 };
