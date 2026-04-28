4c31347f5ff2d494712a47702c2d0f5b3b9ea2d9
Misha Shkodyn <114179592+mishashkodyn@users.noreply.github.com>
2026-04-29 01:36:11 +0300

    Update DependencyInjection.cs

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+using Application.AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
 namespace Application
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
         {
-            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddAutoMapper(typeof(ApplicationUserProfile).Assembly);
 
             return services;
         }
